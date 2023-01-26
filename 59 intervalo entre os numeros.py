72a79b3a6e1d9c851486db8c7b539816874fada4
Marinomura <marinomura@hotmail.com>
2023-01-26 10:58:28 -0400

    26.1

--- 59 intervalo entre os numeros.py
+++ 59 intervalo entre os numeros.py
@@ -1,3 +1,8 @@
 #Faça um programa que receba dois números inteiros 
 # e gere os números inteiros que estão no intervalo compreendido por eles.​
 
+num1=int(input('Digite um número inteiro: '))
+num2=int(input('Digite um número inteiro: '))
+
+for num in range (num1+1,num2):
+  print (num)
