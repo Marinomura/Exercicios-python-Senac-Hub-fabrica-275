15c58790656ac23e390910351ae66a46358217a4
Marinomura <marinomura@hotmail.com>
2023-01-26 11:26:31 -0400

    26.1

--- 61 N calcular e mostrar impares e pares.py
+++ 61 N calcular e mostrar impares e pares.py
@@ -2,13 +2,7 @@
 # a quantidade de números pares e a quantidade de números impares.​
 
 num = int(input("Digite 10 números inteiros "))
-#if(num%2==0):
-#   print("O número digitado é par")
-#else:
-#   print("o número digitado é ímpar")
-    
-#for i in range (1,10):
-#	print (i)
-#print(list(range(1,10)))
 
 
+for i in range(1,10):
+    print (i)
