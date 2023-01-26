cbf4a56dac0e9003130e4ad176cc8585fb596533
Marinomura <marinomura@hotmail.com>
2023-01-26 08:19:48 -0400

    26.1

--- 46 cartorio.py
+++ 46 cartorio.py
@@ -1,9 +1,8 @@
 #Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa.
 # Caso sexo seja “F” e estado civil seja “CASADA”, solicitar o tempo de casada (anos)
 nome= input('Digite o nome: ')
-sexo= input ('Digite o sexo, F para feminino ou M para masculino: ')
-ecivil=input('Digite o estado civil, C para Casada ou S pra Solteira: ')
-if sexo == 'F':
-    else:
+sexo= input ('Digite o sexo, F para feminino ou M para masculino: ').upper()
+ecivil=input('Digite o estado civil, C para Casado ou S pra Solteiro: ').upper()
+if sexo == 'F' and ecivil== 'C':
     print ('Quanto tempo está casada? ')
     
