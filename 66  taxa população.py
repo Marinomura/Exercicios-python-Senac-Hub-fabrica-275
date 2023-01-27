902090c24b2bbeb56c34f8c33a4b33aac520038a
Marinomura <marinomura@hotmail.com>
2023-01-27 10:25:02 -0400

    27.1

--- 66  taxa população.py
+++ 66  taxa população.py
@@ -7,8 +7,8 @@ populaçãoA=80000
 populaçãoB=200000
 ano=0
 while populaçãoA < populaçãoB:
-	populaçãoA+=round((populaçãoA*3.0)/100)
-	populaçãoB+=round((populaçãoB*1.5)/100)
+	populaçãoA +=round((populaçãoA*3.0)/100)
+	populaçãoB +=round((populaçãoB*1.5)/100)
 	ano=ano+1
 
 print("Levará", ano,"anos para  a população da cidade A ser maior que a cidade B")
