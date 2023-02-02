e27c54a99a000881cd235d4b597386a973cb14e2
Marinomura <marinomura@hotmail.com>
2023-02-02 11:29:15 -0400

    02.02.

---
+++ 86.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que receba o valor do salário de uma pessoa e o valor de um financiamento pretendido.
+# Caso o financiamento seja menor ou igual a 5 vezes o salário da pessoa, 
+# o algoritmo deverá escrever “Financiamento Concedido"; senão, ele deverá escrever "Financiamento Negado".
+# Independente de conceder ou não o financiamento, o algoritmo escreverá depois a frase "Obrigado por nos consultar."​
---
+++ 87.py
@@ -0,0 +1,3 @@
+# Crie um algoritmo para uma empresa de transportes que, a partir do peso de uma encomenda fornecida pelo usuário, 
+# calcule o preço da remessa conforme a seguinte tabela:​
+ 
