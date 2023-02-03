7a1051b76d8df67b015afc7bebc90d1005d97b0c
Marinomura <marinomura@hotmail.com>
2023-02-03 11:22:09 -0400

    03.02

--- 92.py
+++ 92.py
@@ -13,14 +13,14 @@
 # O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
 # Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita",
 # Se entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".​
-res = []
-res.append(input("Telefonou para a vítima? 1/Sim ou 0/Não: "))
-res.append(input("Esteve no local do crime? 1/Sim ou 0/Não: "))
-res.append(input("Mora perto da vítima? 1/Sim ou 0/Não: "))
-res.append(input("Devia para a vítima? 1/Sim ou 0/Não: "))
-res.append(input("Já trabalhou com a vítima? 1/Sim ou 0/Não: "))
+respostas = []
+respostas.append(input("Telefonou para a vítima? 1/Sim ou 0/Não: "))
+respostas.append(input("Esteve no local do crime? 1/Sim ou 0/Não: "))
+respostas.append(input("Mora perto da vítima? 1/Sim ou 0/Não: "))
+respostas.append(input("Devia para a vítima? 1/Sim ou 0/Não: "))
+respostas.append(input("Já trabalhou com a vítima? 1/Sim ou 0/Não: "))
 soma_respostas = 0
-for i in res: # soma o número de respostas
+for i in respostas: # soma o número de respostas
     soma_respostas += int(i)
 if (soma_respostas < 2):
  print("\nInocente")
