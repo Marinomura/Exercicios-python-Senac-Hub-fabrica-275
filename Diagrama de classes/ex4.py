026583601b41a97a69f3726bf5f0165f60709db0
Marinomura <marinomura@hotmail.com>
2023-03-20 11:30:51 -0400

    20.3

--- Diagrama de classes/ex4.py
+++ Diagrama de classes/ex4.py
@@ -4,5 +4,5 @@ def pares (lista):
     for num in lista:
         if num%2 == 0:
             par.append (num)
-            return print (par)
+    return print (par)
 pares ([1,2,3,6,8,12,15,19,22])
